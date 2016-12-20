79ea19e22b44e83573bfc827ad6b60dea2774b5a
Chris Nguyen <cngu@users.noreply.github.com>
2016-12-19 19:43:21 -0700

    Add minification&uglification to prod build, and force the build to fail on errors via NoErrorsPlugin

--- build/webpack.config.prod.js
+++ build/webpack.config.prod.js
@@ -1,10 +1,21 @@
+var webpack = require('webpack')
 var merge = require('webpack-merge')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 var baseWebpackConfig = require('./webpack.config.base')
 
 module.exports = merge(baseWebpackConfig, {
+  output: {
+    filename: '[name].min.js'
+  },
   plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.NoErrorsPlugin(),
     new HtmlWebpackPlugin({
       title: 'vue-typer demo',
       template: 'build/template.index.html',
