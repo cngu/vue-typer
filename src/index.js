00024b2faaef7f9fca0b9436fd7960759dcd0f16
Chris Nguyen <cngu@users.noreply.github.com>
2016-12-25 12:19:36 -0700

    Track moved files from previous commit’s source re-organization

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import VueTyperComponent from './components/VueTyper'
-
-export const VueTyper = VueTyperComponent
-
-export default {
-  install(Vue) {
-    Vue.component('vue-typer', VueTyperComponent)
-  }
-}
