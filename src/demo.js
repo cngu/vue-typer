6b9a0e3f29120a8d5c4d3b1d8cce0925ae164b09
Chris Nguyen <cngu@users.noreply.github.com>
2016-12-25 12:20:54 -0700

    Track demo.js > index.js

--- src/demo.js
+++
@@ -1,9 +0,0 @@
-import './styles/demo.css'
-import Vue from 'vue'
-import Demo from './Demo.vue'
-
-// eslint-disable-next-line no-new
-new Vue({
-  el: '#demo',
-  render: createElement => createElement(Demo)
-})
