ed243bf005d988588083d8d5475f9091a0909a02
Chris Nguyen <cngu@users.noreply.github.com>
2016-12-29 02:23:47 -0700

    Clean up loader-util

--- build/loader-util.js
+++ build/loader-util.js
@@ -4,40 +4,43 @@ var pathUtil = require('./path-util.js')
 var vueTyperStyles = pathUtil.getPathFromRoot('src/vue-typer/styles')
 var demoStyles = pathUtil.getPathFromRoot('src/demo/styles')
 
+var cssLoader = 'css-loader?minimize'
+var sassLoader = `sass-loader?includePaths[]=${vueTyperStyles}&includePaths[]=${demoStyles}`
+
 module.exports = {
   getCssLoader: function() {
     if (process.env.NODE_ENV === 'production') {
-      return 'style-loader!css-loader'
+      return `style-loader!${cssLoader}`
     }
     return ExtractTextPlugin.extract({
-      loader: 'css-loader',
+      loader: cssLoader,
       fallbackLoader: 'style-loader'
     })
   },
   getScssLoader: function() {
     if (process.env.NODE_ENV === 'production') {
-      return `style-loader!css-loader!sass-loader?includePaths[]=${vueTyperStyles}&includePaths[]=${demoStyles}`
+      return `style-loader!${cssLoader}!${sassLoader}`
     }
     return ExtractTextPlugin.extract({
-      loader: ['css-loader', `sass-loader?includePaths[]=${vueTyperStyles}&includePaths[]=${demoStyles}`],
+      loader: [cssLoader, `${sassLoader}`],
       fallbackLoader: 'style-loader'
     })
   },
   getVueCssLoader: function() {
     if (process.env.NODE_ENV === 'production') {
-      return 'vue-style-loader!css-loader'
+      return `vue-style-loader!${cssLoader}`
     }
     return ExtractTextPlugin.extract({
-      loader: 'css-loader',
+      loader: cssLoader,
       fallbackLoader: 'vue-style-loader'
     })
   },
   getVueScssLoader: function() {
     if (process.env.NODE_ENV === 'production') {
-      return `vue-style-loader!css-loader!sass-loader?includePaths[]=${vueTyperStyles}&includePaths[]=${demoStyles}`
+      return `vue-style-loader!${cssLoader}!${sassLoader}`
     }
     return ExtractTextPlugin.extract({
-      loader: ['css-loader', `sass-loader?includePaths[]=${vueTyperStyles}&includePaths[]=${demoStyles}`]
+      loader: [cssLoader, `${sassLoader}`]
     })
   }
 }
