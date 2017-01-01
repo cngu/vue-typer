a1ffb68f42255eab54385c8eefa46694f943c3b3
Chris Nguyen <cngu@users.noreply.github.com>
2016-12-31 23:20:56 -0700

    Add alt text to badges

--- src/demo/components/BadgeBar.vue
+++ src/demo/components/BadgeBar.vue
@@ -1,11 +1,11 @@
 <template lang='pug'>
 span.badge-bar
   a(href='https://www.npmjs.com/package/vue-typer', target='_blank')
-    img(src='https://img.shields.io/npm/dt/vue-typer.svg')
+    img(src='https://img.shields.io/npm/dt/vue-typer.svg', alt='Downloads')
   a(href='https://www.npmjs.com/package/vue-typer', target='_blank')
-    img(src='https://img.shields.io/npm/v/vue-typer.svg')
+    img(src='https://img.shields.io/npm/v/vue-typer.svg', alt='Version')
   a(href='https://www.npmjs.com/package/vue-typer', target='_blank')
-    img(src='https://img.shields.io/npm/l/vue-typer.svg')
+    img(src='https://img.shields.io/npm/l/vue-typer.svg', alt='License')
 </template>
 
 <style scoped lang='scss'>
