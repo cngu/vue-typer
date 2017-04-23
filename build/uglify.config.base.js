e3aea37004fbd51ebdead6d7439bc7c543a6fe8b
Chris Nguyen <cngu@users.noreply.github.com>
2017-04-23 16:39:36 -0600

    Provide un-minified version of source

--- build/uglify.config.base.js
+++ build/uglify.config.base.js
@@ -1,7 +1,5 @@
 module.exports = {
-  compress: {
-    warnings: false
-  },
+  test: /\.min\.js$/,
   // Must set this explicitly in webpack 2: https://github.com/shama/webpack-stream/issues/81#issuecomment-243436780
   sourceMap: true
 }
